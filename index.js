20b3eec762d26231098866f5305cb1ff6a9cac18
Latoshia Williams <lswillms@yahoo.com>
2022-07-11 10:51:45 -0500

    commit tree b383fa21725f26ec746491aff7836760e0ee211f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names= ("Karen", "Susan", "Bill")
+const event = "birthday"
+
+function writeCards(cards, event) {
+    let newArray= []
+    for (let i = 0; i < cards.length; i++ ) {
+    newArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray  
+}
+ writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+ function countDown() {
+    let number = 10
+    while (number > -1) {
+      console.log(number--)
+    }
+    return countDown  
+  }
+
+  
+
+
+
